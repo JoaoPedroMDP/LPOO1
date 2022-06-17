a492318fba16d040719d9a2ac62d2ceac2006582
João Pedro Martins de Paula <34918112+JoaoPedroMDP@users.noreply.github.com>
2022-06-17 03:40:45 +0000

    L1 completa

---
+++ L1/EX8/Nome.java
@@ -0,0 +1,9 @@
+package L1.EX8;
+
+public class Nome {
+    public static void main(String[] args) {
+        for(int i = args.length - 1; i >= 0; i--){
+            System.out.println(args[i]);
+        }
+    }
+}
